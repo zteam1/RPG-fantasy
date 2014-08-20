5f62d543f750ac7105ca7b83c2221f962efb3bd2
Moh Hackz <mohackz@live.fr>
2014-08-20 20:22:06 +0200

    Important a lire (click ici)
    tes methodes set_pos et get_pos etaient inutile puisqu'elles sont deja defini dans la classe mere sf::Transformable

--- Entity.hpp
+++ Entity.hpp
@@ -7,14 +7,10 @@ class Entity : public sf::Drawable, public sf::Transformable
 {
     public:
         void set_vel(double velociter);
-        void set_pos(sf::Vector2f position);
 
         double get_vel();
-        sf::Vector2f get_pos();
-
     private:
         double m_velocite;
-        sf::Vector2f m_positions;
 };
 
 #endif // ENTITY_H_INCLUDED
