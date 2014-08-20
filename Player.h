10da52fe113e7a3774c103b760ceb15136701a67
yaziid <cantinanv@hotmail.com>
2014-08-20 21:18:29 +0200

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,19 @@
+#ifndef PLAYER_H_INCLUDED
+#define PLAYER_H_INCLUDED
+
+#include <SFML/Graphics.hpp>
+#include <string>
+#include "Entity.h"
+
+class Player: public Entity
+{
+    public:
+        void set_texture(sf::Texture& texture, std::string img);
+
+    private:
+        sf::Texture m_texture;
+        sf::Sprite m_sprite;
+};
+
+
+#endif // PLAYER_H_INCLUDED
